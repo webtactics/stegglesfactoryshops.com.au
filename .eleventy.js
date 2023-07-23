9682929f830926a227901475603a56873c10d0a5
webtactics <paul@webtactics.com.au>
2023-07-23 15:18:57 +1000

    static

--- .eleventy.js
+++ .eleventy.js
@@ -85,7 +85,7 @@ module.exports = (eleventyConfig) => {
 
   // Don't process folders with static assets e.g. images
   eleventyConfig.addPassthroughCopy("favicon.ico");
-  eleventyConfig.addPassthroughCopy("static/img");
+  eleventyConfig.addPassthroughCopy("static");
   eleventyConfig.addPassthroughCopy("admin/");
   // We additionally output a copy of our CSS for use in Netlify CMS previews
   eleventyConfig.addPassthroughCopy("_includes/assets/css/gtm-grid.css");
